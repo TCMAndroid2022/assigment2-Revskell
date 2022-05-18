a57fb6d0726becc113b91e34313c8649cbbaf354
Joel <joelJRC04@gmail.com>
2022-05-18 12:17:24 +0200

    Starting out

---
+++ README.md
@@ -0,0 +1,5 @@
+## Membres del Grup 11:
+
+Joel Ramos Cano
+Sergio Sánchez González
+Gerard Torrent Castell
---
+++ app/src/main/java/cat/tecnocampus/mobileapps/practica2/JoelRamosCano/SergioSanchezGonzalez/GerardTorrentCastell/MainActivity.java
@@ -0,0 +1,14 @@
+package cat.tecnocampus.mobileapps.practica2.JoelRamosCano.SergioSanchezGonzalez.GerardTorrentCastell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
