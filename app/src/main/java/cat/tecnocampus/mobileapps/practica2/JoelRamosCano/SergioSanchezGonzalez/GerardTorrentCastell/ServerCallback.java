d684e4e607098d4b5e1bb6e3e144d962dfdfa343
@JRC04 <joeljrc04@gmail.com>
2022-06-06 20:22:23 +0200

    Progress on API GET

--- app/src/main/java/cat/tecnocampus/mobileapps/practica2/JoelRamosCano/SergioSanchezGonzalez/GerardTorrentCastell/ServerCallback.java
+++
@@ -1,7 +0,0 @@
-package cat.tecnocampus.mobileapps.practica2.JoelRamosCano.SergioSanchezGonzalez.GerardTorrentCastell;
-
-import org.json.JSONObject;
-
-public interface ServerCallback {
-    void onSuccess(JSONObject response);
-}
